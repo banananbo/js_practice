8dde702305167944a8c428d02ef17548eed53e4a
banananbo <banananbo@gmail.com>
2021-01-31 22:07:37 +0900

    router fix

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,7 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
-import routes from '@src/router.ts';
-
-Vue.use(VueRouter);
-const router = new VueRouter({
-  routes
-});
+import router from '@src/router.ts';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // new Vue(Hello).$mount('#app');
   new Vue({
     el: '#app',
     router: router
--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,14 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 import Home from '@pages/Home.vue';
 import Hello from '@pages/Hello.vue';
 
-export default [
+let routes = [
     { name:"home"  ,path: '/', component: Home },
     { name:"hello" ,path: '/hello', component: Hello },
 ];
+
+Vue.use(VueRouter);
+export default  new VueRouter({
+  routes
+});
